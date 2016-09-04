44ba15fba02154b6b70920f89b28a11a4dfca03d
Brian Lee <writeto.blee@gmail.com>
2016-09-03 23:13:32 -0400

    Move file to new directory

---
+++ src/nyc/fries/EatFries.java
@@ -0,0 +1,11 @@
+package nyc.fries;
+
+/**
+ * Created by BLEED on 9/3/2016.
+ */
+public class EatFries {
+
+    public void eating(){
+        System.out.println("nom");
+    }
+}
---
+++ src/nyc/fries/Main.java
@@ -0,0 +1,9 @@
+package nyc.fries;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
